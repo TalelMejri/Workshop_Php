3b3216668b31b14842a56163bd71123eeaac5192
TalelMejri <talelmejri8@gmail.com>
2023-10-11 22:11:41 +0100

    coonect to bd

--- index.php
+++ index.php
@@ -1,11 +1,37 @@
 <?php 
-    echo "test";
 
+    /*
 
+      $db=new Mysqli("localhost","root","","dsi22todo"); 
+      //Crud OPERATION (create,read,update,delete)
+      $res=$db->query("select * from todos");
+      var_dump($res->fetch_array());
 
+     */
 
+     
 
+     $db=new PDO("mysql:host=localhost;dbname=dsi22todo",'root','');
 
+     // TypeSgbd
+     // host
+     // dbname :database Name
+     // username
+     // password (empty)
 
-    echo "lwees"
+    /* 
+         $res=$db->query("select * from todos");//sql injection
+         $todos=$res->fetchAll();
+         var_dump($todos); 
+     */
+
+     // prepare : Boite Noir
+     // execute : execution
+
+     $res=$db->prepare("select * from todos");
+     $res->execute();
+     $todos=$res->fetchAll();
+     var_dump($todos)
+     
+
 ?>
