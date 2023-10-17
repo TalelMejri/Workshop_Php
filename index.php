afbbe9f945e25faabae86008fe721e575e4ca8c3
TalelMejri <talelmejri8@gmail.com>
2023-10-17 22:50:04 +0100

    afficher list tdodos dans table

--- index.php
+++ index.php
@@ -1,37 +1,41 @@
-<?php 
-
-    /*
-
-      $db=new Mysqli("localhost","root","","dsi22todo"); 
-      //Crud OPERATION (create,read,update,delete)
-      $res=$db->query("select * from todos");
-      var_dump($res->fetch_array());
-
-     */
-
-     
-
-     $db=new PDO("mysql:host=localhost;dbname=dsi22todo",'root','');
-
-     // TypeSgbd
-     // host
-     // dbname :database Name
-     // username
-     // password (empty)
-
-    /* 
-         $res=$db->query("select * from todos");//sql injection
-         $todos=$res->fetchAll();
-         var_dump($todos); 
-     */
-
-     // prepare : Boite Noir
-     // execute : execution
-
-     $res=$db->prepare("select * from todos");
-     $res->execute();
-     $todos=$res->fetchAll();
-     var_dump($todos)
-     
-
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Todos</title>
+</head>
+<body>
+  <div class="container mt-5">
+  <table class="table table-primary ">
+       <thead>
+           <tr>
+              <th>id</th>
+              <th>title</th>
+              <th>desc</th>
+              <th>complete</th>
+              <th>date</th>
+           </tr>
+       </thead>
+       <tbody>
+        <?php 
+              require "./dbConnect.php";
+              $res=$db->prepare("select * from todos");
+              $res->execute();
+              $todos=$res->fetchAll();
+              foreach($todos as $todo):
+          ?>
+           <tr>
+             <td><?php echo $todo['id'] ?></td>
+             <td><?php echo $todo['title'] ?></td>
+             <td><?php echo $todo['description'] ?></td>
+             <td><?php echo $todo['complete'] ?></td>
+             <td><?php echo $todo['date'] ?></td>
+          </tr>
+        <?php endforeach; ?>
+       </tbody>
+   </table>
+  </div>
+</body>
+</html>
